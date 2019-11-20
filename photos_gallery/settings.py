07b540b5435227ef8afe1662d36e4c5bc298c1e6
lethanhtu <lethanhtu1551998@gmail.com>
2019-11-21 02:31:25 +0700

    update

--- photos_gallery/settings.py
+++ photos_gallery/settings.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+print(PROJECT_ROOT)
+
 
 
 # Quick-start development settings - unsuitable for production
