238c2027fb1b72a65cc9adfeb0665f21e5dc410b
lethanhtu <lethanhtu1551998@gmail.com>
2019-11-21 16:36:40 +0700

    tult update display pagination

--- photos/views.py
+++ photos/views.py
@@ -4,21 +4,18 @@ import random
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
+
+
 def home(request):
     i = random.randint(0, 1001)
-    photos = Photo.objects.all()[i: i+50]
+    photos = Photo.objects.all()
     return render(request, 'index.html', {'photos': photos})
+
 
 def photos(request):
     photos_list = Photo.objects.all()
-    page = request.GET.get('page', 1)
-
-    paginator = Paginator(photos_list, 60)
-    try:
-        photos = paginator.page(page)
-    except PageNotAnInteger:
-        users = paginator.page(1)
-    except EmptyPage:
-        users = paginator.page(paginator.num_pages)
+    paginator = Paginator(photos_list, 30)
+    page = request.GET.get('page')  # get the page number
+    photos = paginator.get_page(page)
 
     return render(request, 'photos.html', {'photos': photos})
